aeadeca86777e311f165369cdd321f981aadc353
Elaine R <er@Elaines-MacBook-Air.local>
2014-06-22 12:21:37 -0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/CSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Elaine R on 6/22/14.
+//  Copyright (c) 2014 Emru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/CSViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Elaine R on 6/22/14.
+//  Copyright (c) 2014 Emru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSViewController : UIViewController
+
+@end
